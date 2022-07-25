a561336c72a15392961560649f87f16afbe77234
Kovalenko Vladimir <sazonov2380@gmail.com>
2022-07-25 05:40:34 +0300

    Added card and correct landscope style

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import { Card } from 'antd';
+const { Meta } = Card;
 
 const Services = () => {
   return (
@@ -8,11 +10,30 @@ const Services = () => {
       <p>
         Широкий спект услуг металлообработки, а так же иные сопутствующие работы
       </p>
-      <ul>
+      {/* <div className="site-card-border-less-wrapper">
+      <Card size="small" bordered={false} title="Обработка на станке ЧПУ" extra={<Link to="cnc">Подробнее</Link>} style={{ width: 300 }}>
+      <div><img align="left" src={require("./services/img/topor.jpg")} alt="cnc" height="300px"/></div>
+    </Card>
+    </div> */}
+    <Link to="cnc"><Card
+    style={{ width: 300 }}
+    cover={
+      <img
+        alt="cnc"
+        src={require("./services/img/shesterny.JPG")}
+      />
+    }
+  >
+    <Meta
+      title="Обработка на станке ЧПУ"
+      description="This is the description"
+    />
+  </Card></Link>
+      {/* <ul>
         <li>
           <Link to="cnc">Обработка на станке ЧПУ</Link>
         </li>
-      </ul>
+      </ul> */}
       <Outlet />
     </div>
   );
