8a622b81c57d48bcfd4e0cecc9f3b790bb135b01
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-07-28 06:15:32 +0300

    Restyle header, carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,8 +1,9 @@
-import { Carousel } from "antd";
+import { Carousel, Image } from "antd";
 import "antd/dist/antd.css";
 import React from "react";
 import { Link } from "react-router-dom";
 import { joblist } from "../pages/services/joblist";
+import "./Layout.css";
 /*const contentStyle = {
   height: "100px",
   color: "#fff",
@@ -11,30 +12,19 @@ import { joblist } from "../pages/services/joblist";
   background: "#000"
 };*/
 
-
 const CarouselImg = () => (
   <Carousel autoplay>
     {joblist.map((item) => (
       <div className="joblist" key={item.id}>
-        <Link to="/">
         <div className="img-container">
-          <img align="center" src={item.img} alt={item.title}/>
-          </div>
-        </Link>
+          <Image align="center" src={item.img} alt={item.title} />
+        </div>
+
         {/* <div className="joblist-container">
           <div className="joblist-services">{item.services}</div>
         </div> */}
       </div>
     ))}
-    {/*<div>
-      <h3 style={contentStyle}>2</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>3</h3>
-    </div>
-    <div>
-      <h3 style={contentStyle}>4</h3>
-    </div>*/}
   </Carousel>
 );
 
