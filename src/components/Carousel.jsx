52b1decdc17b909120a89d0f84f16dec9c67e998
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-07-29 16:25:26 +0300

    Added carousel arrow

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,28 +1,22 @@
 import { Carousel, Image } from "antd";
 import "antd/dist/antd.css";
 import React from "react";
-import { Link } from "react-router-dom";
 import { joblist } from "../pages/services/joblist";
+import { LeftCircleTwoTone, RightCircleTwoTone } from "@ant-design/icons";
 import "./Layout.css";
-/*const contentStyle = {
-  height: "100px",
-  color: "#fff",
-  lineHeight: "100px",
-  textAlign: "left",
-  background: "#000"
-};*/
 
 const CarouselImg = () => (
-  <Carousel autoplay>
+  <Carousel
+    autoplay
+    arrows
+    prevArrow={<LeftCircleTwoTone />}
+    nextArrow={<RightCircleTwoTone />}
+  >
     {joblist.map((item) => (
       <div className="joblist" key={item.id}>
         <div className="img-container">
           <Image align="center" src={item.img} alt={item.title} />
         </div>
-
-        {/* <div className="joblist-container">
-          <div className="joblist-services">{item.services}</div>
-        </div> */}
       </div>
     ))}
   </Carousel>
