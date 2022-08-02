4cbf0581a4de1f27ab432b86819ecf6008661e1d
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-08-02 16:50:09 +0300

    process added form

---
+++ src/components/FormCallback.jsx
@@ -0,0 +1,152 @@
+import { Button, Form, Input, Modal, Checkbox, Upload } from "antd";
+import React, { useState } from "react";
+import { UploadOutlined } from "@ant-design/icons";
+
+const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
+  const [form] = Form.useForm();
+  const [componentDisabled, setComponentDisabled] = useState(true);
+
+  const normFile = (e) => {
+    console.log("Upload event:", e);
+
+    if (Array.isArray(e)) {
+      return e;
+    }
+
+    return e?.fileList;
+  };
+
+  return (
+    <Modal
+      visible={visible}
+      title="Отправить файл/запрос для расчёта"
+      okText="Отправить"
+      cancelText="Отмена"
+      onCancel={onCancel}
+      onOk={() => {
+        form
+          .validateFields()
+          .then((values) => {
+            form.resetFields();
+            onCreate(values);
+          })
+          .catch((info) => {
+            console.log("Validate Failed:", info);
+          });
+      }}
+    >
+      <Form
+        form={form}
+        layout="vertical"
+        name="form_in_modal"
+        initialValues={{
+          modifier: "private"
+        }}
+      >
+        <Form.Item
+          name="Name"
+          label="Имя"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста введите ваше имя"
+            }
+          ]}
+        >
+          <Input disabled={componentDisabled} />
+        </Form.Item>
+        <Form.Item
+          name="email"
+          label="E-mail"
+          rules={[
+            {
+              type: "email",
+              message: "The input is not valid E-mail!"
+            },
+            {
+              message: "Please input your E-mail!"
+            }
+          ]}
+        >
+          <Input disabled={componentDisabled} />
+        </Form.Item>
+        <Form.Item
+          name="phone"
+          label="Номер телефона"
+          rules={[
+            {
+              required: true,
+              message: "Пожалуйста, введите номер телефона"
+            }
+          ]}
+        >
+          <Input
+            disabled={componentDisabled}
+            addonBefore="+7"
+            style={{
+              width: "100%"
+            }}
+          />
+        </Form.Item>
+        <Form.Item name="description" label="Описание вопроса">
+          <Input type="textarea" disabled={componentDisabled} />
+        </Form.Item>
+        <Form.Item
+          name="upload"
+          label="Upload"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+          extra="longgggggggggggggggggggggggggggggggggg"
+        >
+          <Upload name="logo" action="/upload.do" listType="picture">
+            <Button icon={<UploadOutlined />}>Click to upload</Button>
+          </Upload>
+        </Form.Item>
+        <Form.Item
+          name="modifier"
+          className="collection-create-form_last-form-item"
+        >
+          <Checkbox
+            checked={!componentDisabled}
+            onChange={(e) => setComponentDisabled(!componentDisabled)}
+          >
+            Нажимая кнопку, я даю своё согласие на обработку моих персональных
+            данных в соответствии с Федеральным законом от 27.07.2006 г. ФЗ "О
+            персональных данных"
+          </Checkbox>
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
+
+const FormCallback = () => {
+  const [visible, setVisible] = useState(false);
+
+  const onCreate = (values) => {
+    console.log("Received values of form: ", values);
+    setVisible(false);
+  };
+
+  return (
+    <div>
+      <Button
+        type="primary"
+        onClick={() => {
+          setVisible(true);
+        }}
+      >
+        New Collection
+      </Button>
+      <CollectionCreateForm
+        visible={visible}
+        onCreate={onCreate}
+        onCancel={() => {
+          setVisible(false);
+        }}
+      />
+    </div>
+  );
+};
+
+export default FormCallback;
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import FormCallback from "../components/FormCallback";
 
 const Contacts = () => {
-  return <div>Добавим контакты в ближайшее время</div>;
+  return (
+    <>
+      <div>Добавим контакты в ближайшее время</div>
+      {/*<FormCallback />*/}
+    </>
+  );
 };
 
 export default Contacts;
