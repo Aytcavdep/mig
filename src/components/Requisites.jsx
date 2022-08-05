a69dbb5664a729977d8f675b60279c6bf068e386
Коваленко Владимир <sazonov2380@gmail.com>
2022-08-05 05:39:24 +0300

    added requisites table

--- src/components/Requisites.jsx
+++ src/components/Requisites.jsx
@@ -1,127 +1,131 @@
 import React from "react";
-
-import { Typography } from 'antd';
+import MyRequisites from "./files/Requisites.docx";
+import { Typography } from "antd";
 import "antd/dist/antd.css";
-import "./Requisites.css"
+import "./Requisites.css";
+import { FileWordTwoTone } from "@ant-design/icons";
 
 const { Paragraph } = Typography;
-
 
 const Requisites = () => {
   return (
     <>
-      {/* <div className="container">
-    <div className="table-head">Реквизиты ООО "Миг"</div>
-    <div className="table">
-      <div>Полное наименование организации</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ЮРИДИЧЕСКИЙ АДРЕС</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>Почтовый адрес</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ИНН</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>КПП</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>БИК</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>Р/С</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>К/С</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ОКПО</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ОКАТО</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ОКВЭД (основной)</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>ОГРН</div>
-      <div>-</div>
-      <div>Генеральный директор</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>Электронная почта</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>Телефон</div>
-      <Paragraph copyable>-</Paragraph>
-      <div>Сайт</div>
-      <Paragraph copyable>-</Paragraph>
-    </div>
-    <div className="table-footer">Скачать реквизиты</div>
-  </div> */}
-  <div className="container">
-  <div className="table-head">Реквизиты ООО "Миг"</div>
-  <table>
-<tbody>
-<tr>
-<td>Полное наименование организации</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ЮРИДИЧЕСКИЙ АДРЕС</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Почтовый адрес</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ИНН</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>КПП</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>БИК</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Р/С</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>К/С</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ОКПО</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ОКАТО</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ОКВЭД (основной)</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>ОГРН</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Генеральный директор</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Электронная почта</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Телефон</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-<tr>
-<td>Сайт</td>
-<td><Paragraph copyable>-</Paragraph></td>
-</tr>
-</tbody>
-</table>
-<div className="table-footer">Скачать реквизиты</div>
-</div>
+      <div className="container">
+        <div className="table-head">Реквизиты ООО "Миг"</div>
+        <table>
+          <tbody>
+            <tr>
+              <td>Полное наименование организации</td>
+              <td>
+                <Paragraph copyable>
+                  Общество с ограниченной ответственностью «МИГ»
+                </Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Краткое наименование</td>
+              <td>
+                <Paragraph copyable>ООО «МИГ»</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Юридический/фактический адрес</td>
+              <td>
+                <Paragraph copyable>
+                  308511 Белгородская область ,Белгородский район, село
+                  Стрелецкое .переулок Королёва , дом 18В
+                </Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Электронная почта</td>
+              <td>
+                <Paragraph copyable>office@migbelg.ru</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Контактный телефон</td>
+              <td>
+                <Paragraph copyable>+7-910-227-47-58</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>ИНН</td>
+              <td>
+                <Paragraph copyable>3100000377</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>КПП</td>
+              <td>
+                <Paragraph copyable>310001001</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>ОГРН</td>
+              <td>
+                <Paragraph copyable>1223100005754</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Руководитель (должность, ФИО)</td>
+              <td>
+                <Paragraph copyable>Очеретный Павел Витальевич</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Наименование банка</td>
+              <td>
+                <Paragraph copyable>АО «ТИНЬКОФФ БАНК»</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>БИК</td>
+              <td>
+                <Paragraph copyable>044525974</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Р/С</td>
+              <td>
+                <Paragraph copyable>40702810210001066981</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>К/С</td>
+              <td>
+                <Paragraph copyable>30101810145250000974</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>ОКПО</td>
+              <td>
+                <Paragraph copyable>93105683</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Система налогообложения</td>
+              <td>
+                <Paragraph copyable>ОСНО</Paragraph>
+              </td>
+            </tr>
+            <tr>
+              <td>Сайт</td>
+              <td>
+                <Paragraph copyable>migbelg.ru</Paragraph>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="table-footer">
+          Скачать реквизиты
+          <a href={MyRequisites} download="OOO_MIG.docx">
+            {" "}
+            <FileWordTwoTone />{" "}
+          </a>
+        </div>
+      </div>
     </>
   );
-  
-}
+};
 export default Requisites;
--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import FormCallback from "../components/FormCallback";
 import Forma from "../components/upload/forma";
+//import {sendMailTo} from "../components/sendMail";
 const Contacts = () => {
+  
   return (
     <div>
       <div>Добавим контакты в ближайшее время</div>
       {/*<FormCallback />
       <Forma />*/}
+      {/* <button type="button" onClick={sendMailTo()}></button> */}
     </div>
   );
 };
