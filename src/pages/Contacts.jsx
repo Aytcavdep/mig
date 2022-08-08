626e4691c515056733e29500a5b9defdba44f3ad
Коваленко Владимир <sazonov2380@gmail.com>
2022-08-08 04:42:07 +0300

    send form testing

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import FormCallback from "../components/FormCallback";
-import sendMailTo from "../components/sendMail";
-import Forma from "../components/upload/forma";
 
 const Contacts = () => {
+  
   return (
     <div>
       <div>Добавим контакты в ближайшее время</div>
-      {/*<FormCallback />
-      <Forma />
-      <button type="button" onClick={() => sendMailTo()}>
-        Нажми меня
-      </button>*/}
+    <FormCallback />
+      
     </div>
   );
 };
