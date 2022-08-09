e7df6fe20d30c7115fcb0b68ce020e5b005a1582
Коваленко Владимир <sazonov2380@gmail.com>
2022-08-09 05:39:47 +0300

    added sender email completed

--- src/components/FormCallback.jsx
+++ src/components/FormCallback.jsx
@@ -1,22 +1,10 @@
-import { Button, Form, Input, Modal, Checkbox, Upload } from "antd";
+import { Button, Modal, Checkbox} from "antd";
 import React, { useState } from "react";
-import { UploadOutlined } from "@ant-design/icons";
-//import FormData from "form-data";
 import axios from "axios";
+import "./FormCallback.css";
 
 const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
-  const [form] = Form.useForm();
   const [componentDisabled, setComponentDisabled] = useState(true);
-
-  const normFile = (e) => {
-    console.log("Upload event:", e);
-
-    if (Array.isArray(e)) {
-      return e;
-    }
-
-    return e?.fileList;
-  };
 
   return (
     <Modal
@@ -26,130 +14,104 @@ const CollectionCreateForm = ({ visible, onCreate, onCancel }) => {
       cancelText="Отмена"
       onCancel={onCancel}
       onOk={() => {
-        form
-          .validateFields()
-          .then((values) => {
-            form.resetFields();
-            onCreate(values);
-          })
-          .catch((info) => {
-            console.log("Validate Failed:", info);
-          });
+        if (componentDisabled) {
+          onCreate();
+        }
+        else {alert("Подтвердите согласие на обработку персональных данных");}
       }}
     >
-      <Form
-        form={form}
-        layout="vertical"
-        name="form_in_modal"
-        initialValues={{
-          modifier: "private"
-        }}
+      <form
+        id="my-form"
+        action="https://migbelg.ru/MailSend/mail.php"
+        method="POST"
+        enctype="multipart/form-data"
       >
-        <Form.Item
-          name="Name"
-          label="Имя"
-          rules={[
-            {
-              required: true,
-              message: "Пожалуйста введите ваше имя"
-            }
-          ]}
+        <div
+          className={componentDisabled ? "form-group" : "form-group disabled"}
         >
-          <Input disabled={componentDisabled} />
-        </Form.Item>
-        <Form.Item
-          name="email"
-          label="E-mail"
-          rules={[
-            {
-              type: "email",
-              message: "The input is not valid E-mail!"
-            },
-            {
-              message: "Please input your E-mail!"
-            }
-          ]}
-        >
-          <Input disabled={componentDisabled} />
-        </Form.Item>
-        <Form.Item
-          name="phone"
-          label="Номер телефона"
-          rules={[
-            {
-              required: true,
-              message: "Пожалуйста, введите номер телефона"
-            }
-          ]}
-        >
-          <Input
-            disabled={componentDisabled}
-            addonBefore="+7"
-            style={{
-              width: "100%"
-            }}
+          <label for="">Имя</label>
+          <input
+            type="text"
+            class="form-control"
+            id=""
+            name="user_name"
+            placeholder="Введите ваше имя"
           />
-        </Form.Item>
-        <Form.Item name="message" label="Описание вопроса">
-          <Input type="textarea" disabled={componentDisabled} />
-        </Form.Item>
-        <Form.Item
-          name="upload"
-          label="Upload"
-          valuePropName="fileList"
-          getValueFromEvent={normFile}
-          extra="longgggggggggggggggggggggggggggggggggg"
+        </div>
+
+        <div
+          className={componentDisabled ? "form-group" : "form-group disabled"}
         >
-          <Upload name="file" /*action="Send_form.php"*/ listType="file">
-            <Button icon={<UploadOutlined />}>Click to upload</Button>
-          </Upload>
-        </Form.Item>
-        <Form.Item
-          name="modifier"
-          className="collection-create-form_last-form-item"
+          <label for="">Номер телефона</label>
+          <input
+            type="text"
+            class="form-control"
+            id=""
+            name="user_phone"
+            placeholder="Введите номер телефона"
+          />
+        </div>
+
+        <div
+          className={componentDisabled ? "form-group" : "form-group disabled"}
         >
-          <Checkbox
-            checked={!componentDisabled}
-            onChange={(e) => setComponentDisabled(!componentDisabled)}
-          >
-            Нажимая кнопку, я даю своё согласие на обработку моих персональных
-            данных в соответствии с Федеральным законом от 27.07.2006 г. ФЗ "О
-            персональных данных"
-          </Checkbox>
-        </Form.Item>
-      </Form>
+          <label for="">E-mail</label>
+          <input type="text" class="form-control" id="" name="user_email" />
+        </div>
+        <div
+          className={componentDisabled ? "form-group" : "form-group disabled"}
+        >
+          <label for="">Сообщение</label>
+          <textarea
+            type="textarea"
+            class="form-control"
+            id=""
+            rows="5"
+            name="user_message"
+          />
+        </div>
+
+        <div
+          className={componentDisabled ? "form-group" : "form-group disabled"}
+        >
+          <label for=""></label>
+          <input type="file" id="file" name="upload" />
+        </div>
+
+        <Checkbox
+          checked={componentDisabled}
+          onChange={(e) => setComponentDisabled(!componentDisabled)}
+        >
+          Нажимая кнопку, я даю своё согласие на обработку моих персональных
+          данных в соответствии с Федеральным законом от 27.07.2006 г. ФЗ "О
+          персональных данных"
+        </Checkbox>
+      </form>
     </Modal>
   );
 };
 
 const FormCallback = () => {
   const [visible, setVisible] = useState(false);
-
+  let myForm;
   const onCreate = (values) => {
-    console.log("Received values of form: ", values);
+    myForm = document.getElementById("my-form");
     uploadData(values);
     setVisible(false);
+    
   };
   const FormData = require("form-data");
+  
   const uploadData = (values) => {
+    let fd = new FormData(myForm);
     axios
-      .post("mail.php", values, {
+      .post("https://migbelg.ru/MailSend/mail.php", fd, {
         headers: {
           "Content-Type": "multipart/form-data"
         }
       })
       .then(({ data }) => console.log(data));
-    /* let fd = new FormData();
-/*for (const file of values.upload) { // images is an array of File Object
-  fd.append('file', file, file.name); // multiple upload
-}*/
-
-    /*async function responce(){ await axios.post(
-  "Send_form.php", fd,
-  { headers: fd.getHeaders() }
-);
-console.log(responce);
-}*/
+    myForm.reset();
   };
 
   return (
@@ -160,7 +122,7 @@ console.log(responce);
           setVisible(true);
         }}
       >
-        New Collection
+        Отправить сообщение
       </Button>
       <CollectionCreateForm
         visible={visible}
